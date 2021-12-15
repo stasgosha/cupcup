1c80286efc39c921dfb4bfd1b78bd7a4036434ec
Stas Goshovsky <stasgosha@yandex.ru>
2021-12-16 01:01:17 +0200

    - desktop done

--- js/scripts.js
+++ js/scripts.js
@@ -125,8 +125,14 @@ document.addEventListener('DOMContentLoaded', function(){
 	}
 
 	let arrowsButtons = {
-		prevArrow: '<button type="button" class="slick-arrow slick-prev" aria-label="Previous"><svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 37"><path d="M20 1 1 17.634 18.667 36"/></svg></button>',
-		nextArrow: '<button type="button" class="slick-arrow slick-next" aria-label="Next"><svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 37"><path d="m1 1 19 16.634L2.333 36"/></svg></button>'
+		prevArrow: `<button type="button" class="slick-arrow slick-prev" aria-label="Previous">
+						<svg class="btn-icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path d="m19.47 29.03-8.94-8.95H30.5v-4.16H10.53l8.94-8.95-2.94-2.94L2.55 18l13.98 13.97 2.94-2.94Z" fill="#fff"/></svg>
+						<svg class="btn-outline" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 62 40"><path d="M9.25 14.08c-.78.93-13.98 11.82-4.92 19.53 9.06 7.72 52.11 7.24 55.83-15.35C63.87-4.34 19.64.66 14.76 4.9" stroke="#00DAD0" stroke-width="2" stroke-linecap="round"/></svg>
+					</button>`,
+		nextArrow: `<button type="button" class="slick-arrow slick-next" aria-label="Next">
+						<svg class="btn-icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path d="m16.53 29.03 2.94 2.94L33.45 18 19.47 4.03l-2.94 2.94 8.94 8.95H5.5v4.16h19.97l-8.94 8.95Z" fill="#fff"/></svg>
+						<svg class="btn-outline" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 62 40"><path d="M9.25 14.08c-.78.93-13.98 11.82-4.92 19.53 9.06 7.72 52.11 7.24 55.83-15.35C63.87-4.34 19.64.66 14.76 4.9" stroke="#00DAD0" stroke-width="2" stroke-linecap="round"/></svg>
+					</button>`
 	}
 
 	$('.first-screen-slider').slick({
@@ -135,9 +141,72 @@ document.addEventListener('DOMContentLoaded', function(){
 		slidesToScroll: 1,
 		arrows: false,
 		dots: false,
-		...arrowsButtons,
 		autoplay: true,
 		autoplaySpeed: 2000
+	});
+
+	$('.products-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		autoplay: true,
+		autoplaySpeed: 1500
+	});
+
+	$('.reviews-slider-scope').each(function(i, scope){
+		const slider = $(scope).find('.reviews-slider');
+
+		slider.on('init', function (e, slick) {
+			$(scope).find('.slider-nav .current').text('1');
+			$(scope).find('.slider-nav .total').text(slick.slideCount);
+		});
+
+		slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+			$(scope).find('.slider-nav .current').text(nextSlide + 1);
+		});
+
+		slider.slick({
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			arrows: true,
+			dots: false,
+			autoplay: false,
+			...arrowsButtons,
+			appendArrows: $(scope).find('.slider-nav')
+		});
+
+		equalSlideHeight( slider );
+	});
+
+	$('.about-delivery-component').each(function(i, cmp){
+		const slider = $(cmp).find('.cmp-slider');
+		const sliderNav = $(cmp).find('.cmp-nav');
+
+		slider.on('init', function (e, slick) {
+			sliderNav.find('[data-slide="0"]').addClass('active');
+		});
+
+		slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+			sliderNav.find('[data-slide]').removeClass('active').filter(`[data-slide="${nextSlide}"]`).addClass('active');
+		});
+
+		slider.slick({
+			infinite: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			dots: false,
+			fade: true
+		});
+
+		sliderNav.find('[data-slide]').click(function(e){
+			e.preventDefault();
+
+			slider.slick('slickGoTo', +$(this).data('slide'));
+		});
 	});
 
 	// Scroll to anchor
